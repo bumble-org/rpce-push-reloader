0bca0b6c4e66493c4e278f4968a9fa574da0f578
Jack Steam <teamsteamdev@gmail.com>
2019-10-07 17:01:06 -0500

    Make firebase an external dep

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,14 @@
 
 import bundleImports from 'rollup-plugin-bundle-imports'
 import typescript from 'rollup-plugin-typescript'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
+// import commonjs from 'rollup-plugin-commonjs'
+// import resolve from 'rollup-plugin-node-resolve'
 
 const plugins = [
   typescript(),
-  resolve(),
-  commonjs(),
+  // Was going to try to bundle firebase...
+  // resolve(),
+  // commonjs(),
   bundleImports({
     include: ['**/*.sw.js'],
     importAs: 'path',
@@ -34,11 +35,11 @@ export default [
         sourcemap: 'inline',
       },
     ],
-    // external: [
-    //   'firebase/app',
-    //   'firebase/auth',
-    //   'firebase/functions',
-    // ],
+    external: [
+      'firebase/app',
+      'firebase/auth',
+      'firebase/functions',
+    ],
     plugins,
   },
 ]
